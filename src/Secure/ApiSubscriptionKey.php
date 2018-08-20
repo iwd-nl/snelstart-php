2854958ba5ec4c80a9d3c34c1b5845773b0925e1
Manuel de Ruiter <info@intowebdevelopment.nl>
2018-08-20 14:33:17 +0200

    A little more boilerplate but this can come handy in the future

---
+++ src/Secure/ApiSubscriptionKey.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author  IntoWebDevelopment <info@intowebdevelopment.nl>
+ * @project SnelstartApiPHP
+ *
+ * I know.. A lot of boilerplate but we can change this in the near future.
+ */
+
+namespace SnelstartPHP\Secure;
+
+class ApiSubscriptionKey implements \IteratorAggregate
+{
+    /**
+     * @var array
+     */
+    private $keys;
+
+    public function __construct(string $primaryKey, string $secondaryKey)
+    {
+        $this->keys[] = $primaryKey;
+        $this->keys[] = $secondaryKey;
+    }
+
+    public function getPrimary(): string
+    {
+        return $this->keys[0];
+    }
+
+    public function getSecondary(): string
+    {
+        return $this->keys[1];
+    }
+
+    public function getKeys(): array
+    {
+        return $this->keys;
+    }
+
+    public function getIterator(): \Iterator
+    {
+        return new \ArrayIterator($this->getKeys());
+    }
+}
