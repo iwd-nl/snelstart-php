d1c2067161f63e8ce01ab94a69cbe10289398fd6
Manuel de Ruiter <manuel@inquiry.center>
2020-01-11 19:25:41 +0100

    Added some basic getters and setters to ODataRequestDataInterface

--- src/Request/ODataRequestDataInterface.php
+++ src/Request/ODataRequestDataInterface.php
@@ -4,5 +4,17 @@ namespace SnelstartPHP\Request;
 
 interface ODataRequestDataInterface
 {
+    public function getFilter(): array;
+
+    public function getApply(): array;
+
+    public function setTop(int $top): ODataRequestDataInterface;
+
+    public function getTop(): int;
+
+    public function setSkip(int $skip): ODataRequestDataInterface;
+
+    public function getSkip(): int;
+
     public function getHttpCompatibleQueryString(): string;
 }
