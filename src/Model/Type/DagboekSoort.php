c87d8ad75ea673f9b5e47651afab10583bb64f4f
luki <szymon@storekeeper.nl>
2021-09-09 13:45:33 +0200

    Added Missing DagboekSoort:Geen

--- src/Model/Type/DagboekSoort.php
+++ src/Model/Type/DagboekSoort.php
@@ -6,6 +6,7 @@ use MyCLabs\Enum\Enum;
 /**
  * @psalm-immutable
  *
+ * @method static DagboekSoort GEEN()
  * @method static DagboekSoort KAS()
  * @method static DagboekSoort BANK()
  * @method static DagboekSoort VERKOOP()
@@ -15,10 +16,11 @@ use MyCLabs\Enum\Enum;
  */
 class DagboekSoort extends Enum
 {
+    private const GEEN    = 'Geen';
     private const KAS      = 'Kas';
     private const BANK      = 'Bank';
     private const VERKOOP      = 'Verkoop';
     private const INKOOP    = 'Inkoop';
-    private const BALANS    = 'Balans';
     private const MEMORIAAL    = 'Memoriaal';
+    private const BALANS    = 'Balans';
 }
