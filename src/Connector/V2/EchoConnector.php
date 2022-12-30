f6bd7b9234a0dd591e8a4719c12823a80701c32e
Harry van der Valk <info@hvsoftware.nl>
2022-12-30 14:26:29 +0100

    Remove redundant cast to string

--- src/Connector/V2/EchoConnector.php
+++ src/Connector/V2/EchoConnector.php
@@ -14,6 +14,6 @@ final class EchoConnector extends BaseConnector
 {
     public function echo(string $input): string
     {
-        return (string) str_replace('"', "", $this->connection->doRequest((new EchoRequest())->echo($input))->getBody()->getContents());
+        return str_replace('"', "", $this->connection->doRequest((new EchoRequest())->echo($input))->getBody()->getContents());
     }
 }
