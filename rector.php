28227dc77f9f68c7634d729142c0a27d7e247f68
Harry van der Valk <info@hvsoftware.nl>
2023-06-29 20:04:51 +0200

    Update rector to update the code to PHP 7.4
    All applied rules are skipped. The team need to decide which rules they want to apply and which to skip

--- rector.php
+++ rector.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\Php74\Rector\Assign\NullCoalescingOperatorRector;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -17,6 +20,12 @@ return static function (RectorConfig $rectorConfig): void {
 
     // define sets of rules
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_73,
+        LevelSetList::UP_TO_PHP_74,
+    ]);
+
+    $rectorConfig->skip([
+        TypedPropertyFromAssignsRector::class,
+        NullCoalescingOperatorRector::class, // https://wiki.php.net/rfc/null_coalesce_equal_operator
+        ClosureToArrowFunctionRector::class, // https://wiki.php.net/rfc/arrow_functions_v2
     ]);
 };
