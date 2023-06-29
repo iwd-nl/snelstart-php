985cf9391be6447a5e989547da6db2f3fe71c067
Harry van der Valk <info@hvsoftware.nl>
2023-06-29 20:16:20 +0200

    Update code to support phpunit 8

--- rector.php
+++ rector.php
@@ -6,6 +6,7 @@ use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Php74\Rector\Assign\NullCoalescingOperatorRector;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
 
@@ -21,11 +22,12 @@ return static function (RectorConfig $rectorConfig): void {
     // define sets of rules
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_74,
+        PHPUnitSetList::PHPUNIT_80,
     ]);
 
     $rectorConfig->skip([
-        TypedPropertyFromAssignsRector::class,
-        NullCoalescingOperatorRector::class, // https://wiki.php.net/rfc/null_coalesce_equal_operator
-        ClosureToArrowFunctionRector::class, // https://wiki.php.net/rfc/arrow_functions_v2
+//        TypedPropertyFromAssignsRector::class,
+//        NullCoalescingOperatorRector::class, // https://wiki.php.net/rfc/null_coalesce_equal_operator
+//        ClosureToArrowFunctionRector::class, // https://wiki.php.net/rfc/arrow_functions_v2
     ]);
 };
